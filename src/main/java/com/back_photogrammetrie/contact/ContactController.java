00ffe56342e49a0289b7518fde14b5ae9ec5cfcf
HenriBoulnois <boulnois.henri@gmail.com>
2022-02-13 21:29:26 +0100

    can delete a message

--- src/main/java/com/back_photogrammetrie/contact/ContactController.java
+++ src/main/java/com/back_photogrammetrie/contact/ContactController.java
@@ -3,7 +3,9 @@ package com.back_photogrammetrie.contact;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +30,11 @@ public class ContactController {
 	public Contact addContact(@RequestBody Contact newContact) {
 		return contactService.addContact(newContact);
 	}
+	
+	@DeleteMapping("/delete/{messageId}")
+    public void getUser(@PathVariable("messageId") int messageId) {
+    	if(contactService.allContacts().get(messageId)!=null) {
+    		contactService.allContacts().remove(messageId);
+    	}
+    }
 }
